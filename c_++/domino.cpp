d46bf81bee841c6d8882ce08e51fea8f3cbe91b0
Alfonso Barajas <63176184+AlfonsBC@users.noreply.github.com>
2021-09-29 23:41:33 -0500

    Adding problems solved with c++

---
+++ c_++/domino.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int m,n;
+    cin>>m>>n;
+    int squares = m*n;
+    cout<< squares/2;
+}
+
+//https://codeforces.com/contest/50/submission/130192865
