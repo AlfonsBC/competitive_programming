74600fffffb2ed599434eacd2f5ab5050bb83ccb
AlfonsBC <albace2000@hotmail.com>
2021-10-13 21:57:17 -0500

    Adding arrays

---
+++ algoritmia_club/arrays.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x = 1005;
+    // Declarar un apuntador 
+    // obtener el valor del apuntador
+    int* ap_x = &x; 
+    // Obtener dirección de memoria de una variable
+    // Marcar como referencia una variable
+    cout << ap_x << endl;
+
+}
